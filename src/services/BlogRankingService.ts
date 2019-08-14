6470140f1ecc123299e3e0ba8af3adf27352ff08
Ryuichi Sakagami <shagamiiiii@gmail.com>
2019-08-14 17:19:45 +0900

    fix

--- src/services/BlogRankingService.ts
+++ src/services/BlogRankingService.ts
@@ -1,6 +1,4 @@
-import fetch from "node-fetch";
-
-class RankingService {}
+class RankingService { }
 
 type Genre = string;
 
@@ -26,24 +24,25 @@ export class BlogRankingService extends RankingService {
   }
 
   private async fetch() {
-    try {
-      const res = await fetch(
-        `https://blogger.ameba.jp/api/blogs/rankings?genreCode=${
-          this.genre
-        }&limit=10&includeAmebaBlog=true&includeAmebaBlogRecentEntries=true`
-      );
-      return res.json();
-    } catch (e) {
-      return new Error(e);
-    }
+    return Promise.resolve({
+      data: [{
+        nickname: 'shagamii',
+        profileImage: 'https://shagamii.tech/static/portrait-b8a95406c40399ec413dffdc92d627af.png',
+        title: 'This is my profile!'
+      },
+      {
+        nickname: 'taro',
+        profileImage: 'https://4.bp.blogspot.com/-07kP2lUa7Fg/XLAdf6K1E9I/AAAAAAABSY8/E9DvKUWrdnc51az3JqtT6PNoFS8xm3qAQCLcBGAs/s180-c/sweets_peropero_candy_boy.png',
+        title: "This is taro's profile!"
+      }]
+    })
   }
 
   async getEntries() {
     if (this.entries === null) {
       const rawEntries = await this.fetch();
-      // @ts-ignore
       this.entries = rawEntries.data.map(data => ({
-        blog: data.amebaBlog
+        blog: data
       }));
     }
     return this.entries;
--- src/views/pages/BlogRanking.tsx
+++ src/views/pages/BlogRanking.tsx
@@ -19,36 +19,6 @@ export function BlogRanking(props: Props) {
       publisher-logo-src="http"
       poster-portrait-src="https"
     >
-      <Amp.AmpStoryPage id="cover-image" auto-advance-after="2.5s">
-        <Amp.AmpStoryGridLayer template="fill">
-          <CoverContainer>
-            <CoverIconContainer
-              id="cover-icon"
-              animate-in="drop"
-              animate-in-duration="1s"
-              animate-in-delay="0.5s"
-            >
-              <div
-                animate-in="pulse"
-                animate-in-duration="0.8s"
-                animate-in-after="cover-icon"
-                animate-in-delay="0.2s"
-              >
-                <AmebaIcon specName="default" width="200px" height="200px" />
-              </div>
-            </CoverIconContainer>
-          </CoverContainer>
-        </Amp.AmpStoryGridLayer>
-        <Amp.AmpStoryPageAttachment layout="nodisplay">
-          <h1>My title</h1>
-          <p>
-            Lots of interesting text with{" "}
-            <a href="https://example.ampproject.org">links</a>!
-          </p>
-          <p>More text and a YouTube video!</p>
-          <p>And a tweet!</p>
-        </Amp.AmpStoryPageAttachment>
-      </Amp.AmpStoryPage>
       {entries.map(({ blog }) => (
         <Amp.AmpStoryPage id={blog.nickname} key={blog.nickname}>
           <Amp.AmpStoryGridLayer template="fill">
@@ -85,30 +55,4 @@ const UserIcon = styled(Amp.AmpImg).attrs({
 
 const Title = styled(Amp.H1)`
   background-color: rgba(255, 255, 255, 0.8);
-`;
-
-const CoverContainer = styled(Amp.Div).attrs({
-  specName: "default"
-})`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  background-color: #2d8c3c;
-`;
-
-const CoverIconContainer = styled(Amp.Div).attrs({
-  specName: "default"
-})`
-  border-radius: 50%;
-  background-color: white;
-  padding: 20px;
-`;
-
-const AmebaIcon = styled(Amp.AmpImg).attrs({
-  layout: "fixed",
-  src:
-    "http://stat.ameba.jp/user_images/20150721/18/staff-amebame/14/a3/p/o0200020013372500852.png"
-})`
-  border-radius: 50%;
 `;
